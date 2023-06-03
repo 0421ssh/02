32264ce297a07302eab915425e87d02531a76ad1
0421ssh <135157606+0421ssh@users.noreply.github.com>
2023-06-04 00:38:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -104,12 +104,6 @@ def get_birthday(birthday, year, today):
  
 def get_ciba():
     url = "http://open.iciba.com/dsapi/"
-    headers = {
-        'Content-Type': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
-    }
-    r = get(url, headers=headers)
     note_en = r.json()["content"]
     note_ch = r.json()["note"]
     return note_ch, note_en
